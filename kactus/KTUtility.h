df54e16238ee321b5aef7065690fcec741fb05a5
Federico Polesello <federico.polesello@metide.com>
2015-02-26 10:30:21 +0100

    new classes

---
+++ kactus/KTUtility.h
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface KTUtility : NSObject
+
++ (BOOL) validateEmail:(NSString *)email;
++ (BOOL) validatePassword:(NSString *)password;
++ (void) showActivityIndicatorToView:(UIView*)view withType:(UIActivityIndicatorViewStyle)activityIndicatorViewStyle;
++ (void) hideActivityIndicator:(UIView*)view;
++ (UIImageView*) showCustomIndicator:(UIView*)view;
++ (NSString*) dictToJSONString:(NSDictionary*)dict;
++ (NSMutableDictionary*) JSONStringtoDictionary:(NSString*)jsonString;
++ (NSMutableArray*) JSONStringtoArray:(NSString*)jsonString;
++ (NSString*) truncateString:(NSString *)stringToTruncate after:(int)maxRange;
++ (UIView*)addGradientViewToBackView:(UIView*)viewParent withFrame:(CGRect)frameGradientView colors:(NSArray*)arrayColors andGradientLocation:(NSArray*)arrayGradientLocation;
+
+@end
