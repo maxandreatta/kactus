496538c9f5b86cf912e554b85c64e013f97e91ee
Federico Polesello <federico.polesello@metide.com>
2015-03-23 11:30:42 +0100

    fix utility class

--- kactus/KTUtility.h
+++ kactus/KTUtility.h
@@ -3,15 +3,15 @@
 
 @interface KTUtility : NSObject
 
-+ (BOOL) validateEmail:(NSString *)email;
-+ (BOOL) validatePassword:(NSString *)password;
-+ (void) showActivityIndicatorToView:(UIView*)view withType:(UIActivityIndicatorViewStyle)activityIndicatorViewStyle;
-+ (void) hideActivityIndicator:(UIView*)view;
-+ (UIImageView*) showCustomIndicator:(UIView*)view;
-+ (NSString*) dictToJSONString:(NSDictionary*)dict;
-+ (NSMutableDictionary*) JSONStringtoDictionary:(NSString*)jsonString;
-+ (NSMutableArray*) JSONStringtoArray:(NSString*)jsonString;
-+ (NSString*) truncateString:(NSString *)stringToTruncate after:(int)maxRange;
++ (BOOL)validateEmail:(NSString *)email;
++ (BOOL)validatePassword:(NSString *)password;
++ (void)showActivityIndicatorToView:(UIView*)view withType:(UIActivityIndicatorViewStyle)activityIndicatorViewStyle;
++ (void)hideActivityIndicator:(UIView*)view;
++ (UIImageView*)showCustomIndicator:(UIView*)view;
++ (NSString*)dictToJSONString:(NSDictionary*)dict;
++ (NSMutableDictionary*)JSONStringtoDictionary:(NSString*)jsonString;
++ (NSMutableArray*)JSONStringtoArray:(NSString*)jsonString;
++ (NSString*)truncateString:(NSString *)stringToTruncate after:(int)maxRange;
 + (UIView*)addGradientViewToBackView:(UIView*)viewParent withFrame:(CGRect)frameGradientView colors:(NSArray*)arrayColors andGradientLocation:(NSArray*)arrayGradientLocation;
 
 @end
--- kactus/KTUtility.m
+++ kactus/KTUtility.m
@@ -2,16 +2,16 @@
 
 @implementation KTUtility
 
-+(BOOL) validateEmail: (NSString *) email 
-{
++ (BOOL)validateEmail:(NSString *)email {
+    
 	NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
 	NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
 	BOOL isValid = [emailTest evaluateWithObject:email];
 	return isValid;
+    
 }
 
-+(BOOL) validatePassword: (NSString *) password
-{
++ (BOOL)validatePassword:(NSString *)password {
     // Tra 8 e 10 caratteri, almeno 1 maiuscola, almeno 1 minuscola e almeno un numero
     NSCharacterSet *lowerCaseChars = [NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyz"];
     
@@ -32,55 +32,86 @@
         return true;
 }
 
-+(NSString*) dictToJSONString:(NSDictionary*) dict {
++ (NSString*)dictToJSONString:(NSDictionary*)dict {
+    
     NSError *error;
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options:0 error:&error];
+
     if (!jsonData) {
+        
         NSLog(@"JSON error: %@", error);
+        return  nil;
+        
     } else {
         
         NSString *JSONString = [[NSString alloc] initWithBytes:[jsonData bytes] length:[jsonData length] encoding:NSUTF8StringEncoding];
-        NSLog(@"JSON OUTPUT: %@",JSONString);
+        // NSLog(@"JSON OUTPUT: %@",JSONString);
         return JSONString;
+        
     }
-    return  nil;
+    
 }
 
-+(NSMutableDictionary*) JSONStringtoDictionary:(NSString*) jsonString {
++ (NSMutableDictionary*)JSONStringtoDictionary:(NSString*)jsonString {
+    
     NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
     NSError *error = nil;
+    
     if(jsonString.length != 0) {
+        
         NSMutableDictionary *myDictionary = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&error];
+        
         if(!myDictionary) {
+            
             NSLog(@"%@",error);
             return  nil;
+            
         } else {
+            
             return myDictionary;
+            
         }
+        
     } else {
-        NSLog(@"ricevuta stringa vuota");
+        
+        NSLog(@"empty string");
         return  nil;
+        
     }
+    
 }
 
-+(NSMutableArray*) JSONStringtoArray:(NSString*) jsonString {
++ (NSMutableArray*)JSONStringtoArray:(NSString*)jsonString {
+    
     NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
     NSError *error = nil;
+    
     if(jsonString.length != 0) {
+        
         NSMutableArray *arrayNewsByString = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&error];
+
         if(!arrayNewsByString) {
+            
             NSLog(@"%@",error);
             return  nil;
+            
         } else {
+            
             return arrayNewsByString;
+            
         }
+        
     } else {
-        NSLog(@"ricevuta stringa vuota");
+        
+        NSLog(@"empty string");
         return  nil;
+        
     }
+    
 }
 
 + (void)showActivityIndicatorToView:(UIView*)view withType:(UIActivityIndicatorViewStyle)activityIndicatorViewStyle {
+    
     UIActivityIndicatorView *activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:activityIndicatorViewStyle];
     
     activityView.center = view.center;
@@ -93,45 +124,65 @@
     
     [activityView startAnimating];
     [view addSubview:activityView];
+    
 }
 
 + (void) hideActivityIndicator:(UIView*)view {
+    
     UIView *activityView;
     NSEnumerator *subviewsEnum = [view.subviews reverseObjectEnumerator];
-	for (UIView *subview in subviewsEnum) {
+
+    for (UIView *subview in subviewsEnum) {
+        
 		if ([subview isKindOfClass:[UIActivityIndicatorView class]]) {
+            
 			activityView = subview;
             break;
+            
 		}
+        
 	}
+
     if(activityView) {
+        
         [activityView removeFromSuperview];
         activityView = nil;
+        
     }
+    
 }
 
 + (UIImageView*)showCustomIndicator:(UIView*)view {
+    
     UIImageView* imageLoadingFrame = [[UIImageView alloc] initWithFrame:view.bounds];
     NSMutableArray* arrayTempImage = [NSMutableArray array];
+    
     for (int iIndex = 0; iIndex < 8; iIndex++) {
+    
         NSString *nameImage = [NSString stringWithFormat:@"%04i", iIndex];
         nameImage = [NSString stringWithFormat:@"loader_%@", nameImage];
         [arrayTempImage addObject:[UIImage imageNamed:nameImage]];
+        
     }
+    
     imageLoadingFrame.animationDuration = 1;
     imageLoadingFrame.contentMode = UIViewContentModeCenter;
     // imageLoadingFrame.backgroundColor = [UIColor colorWithHexString:@"FFFFFF" alpha:0.5];
     imageLoadingFrame.animationImages = [NSArray arrayWithArray:arrayTempImage];
     return imageLoadingFrame;
+    
 }
 
 + (NSString*)truncateString:(NSString *)stringToTruncate after:(int)maxRange {
+    
     NSRange stringRange = {0, MIN([stringToTruncate length], maxRange)};
     stringRange = [stringToTruncate rangeOfComposedCharacterSequencesForRange:stringRange];
     NSString *shortString = [stringToTruncate substringWithRange:stringRange];
+    
     if([stringToTruncate length] > maxRange)
         shortString = [NSString stringWithFormat:@"%@...", shortString];
     return shortString;
+    
 }
 
 /**
